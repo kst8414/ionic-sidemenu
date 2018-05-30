b2e038aa3192d61fa98250a814feb741984c00bd
kst8414 <kst8414@hanmail.net>
2018-05-30 21:17:06 +0900

    action sheet 추가, modal page 추가

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {ModalController, NavController} from 'ionic-angular';
+import {Profile} from "../../interface/Profile";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public profile = {} as Profile;
+  constructor(public navCtrl: NavController,
+              public modalCtrl: ModalController) {
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  modal() {
+    let modal = this.modalCtrl.create('ModalPage');
+    modal.onDidDismiss(data => {
+      if(Object.keys(data).length === 0){
+        console.log(data);
+      }else {
+        this.profile.actionSwitch = data.actionSwitch;
+        this.profile.name = data.name;
+        this.profile.gender = data.gender;
+        this.profile.domestic = data.domestic;
+        this.profile.startDate = data.startDate;
+      }
+    });
+    modal.present();
   }
 
 }
